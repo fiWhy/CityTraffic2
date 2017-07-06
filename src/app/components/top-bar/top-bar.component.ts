ba24e41e508dba105edc6db3a280f4c76abc37da
Denis <fiwhy@mail.ru>
2017-07-06 12:02:28 +0300

    Local components for app
    #Create local components structure for app
    #Create top bar component

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+  styleUrls: ['./top-bar.component.scss'],
 })
 export class TopBarComponent implements OnInit {
-
+  @Input() leftSideNavId;
   constructor() { }
 
   ngOnInit() {
